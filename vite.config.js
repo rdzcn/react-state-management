ffd04945bbaa327f60f7c92d43a1fc140a5bf5d2
rdzcn <ardaoezcan@gmail.com>
2023-01-18 22:38:16 +0100

    implement prettier

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: "build",
+  },
+  server: {
+    port: 3000,
+  },
   plugins: [react()],
 })
