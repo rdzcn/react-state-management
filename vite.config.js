0fb6bd44b56160afed25ce609a2725f6801a569e
rdzcn <ardaoezcan@gmail.com>
2023-01-28 22:08:37 +0100

    change config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: "build",
+  },
+  server: {
+    port: 8080,
+  },
   plugins: [react()],
-})
+});
