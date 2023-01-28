a1d2d0e13acb2bf1d79ec2fc166929bc9df226ba
rdzcn <ardaoezcan@gmail.com>
2023-01-28 22:08:37 +0100

    config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,7 +7,7 @@ export default defineConfig({
     outDir: "build",
   },
   server: {
-    port: 3000,
+    port: 8080,
   },
   plugins: [react()],
-})
+});
