c0d5e61ba3b247d628becd2a59ce6bb3068e9367
rdzcn <ardaoezcan@gmail.com>
2023-01-31 19:43:47 +0100

    update className

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <h1 style={{ marginBottom: "48px" }}>Hello State Management</h1>
-      <div class="components-container">
+      <div className="components-container">
         <MyComponent />
       </div>
     </div>
