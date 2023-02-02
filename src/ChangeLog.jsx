91dafda71b535cf0a641cfa557c41e9064e0f66d
rdzcn <ardaoezcan@gmail.com>
2023-02-02 16:50:24 +0100

    update change log

--- src/ChangeLog.jsx
+++ src/ChangeLog.jsx
@@ -4,6 +4,7 @@ export default function ChangeLog() {
       <h4>Change log</h4>
       <ul className="change-log-list">
         <li className="change-log-item">Memo components</li>
+        <li className="change-log-item">useReducer with finite state</li>
       </ul>
     </div>
   );
