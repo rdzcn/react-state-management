43ac8956c74f3ed63403b48fa5899d4db73c6937
rdzcn <ardaoezcan@gmail.com>
2023-02-02 11:33:45 +0100

    update change log

--- src/ChangeLog.jsx
+++ src/ChangeLog.jsx
@@ -3,8 +3,8 @@ export default function ChangeLog() {
     <div className="change-log-wrapper">
       <h4>Change log</h4>
       <ul className="change-log-list">
-        <li className="change-log-item">Add new isYellow state</li>
-        <li className="change-log-item">Update states correctly</li>
+        <li className="change-log-item">Consolidate 3 states into 1</li>
+        <li className="change-log-item">Update states correctly in click handlers</li>
       </ul>
     </div>
   )
